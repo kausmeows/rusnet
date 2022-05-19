bf1b3ab169fe09859cf51b13300730ad770d1b39
kaustubh-s1 <shuklakaustubh84@gmail.com>
2022-05-19 20:25:14 +0530

    implement a get route to receive messages

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-#[macro_use]extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use rocket::{State, Shutdown};
-use rocket::fs::{relative, FileServer};
 use rocket::form::Form;
-use rocket::response::stream::{EventStream, Event};
-use rocket::serde::{Serialize, Deserialize};
-use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
+use rocket::fs::{relative, FileServer};
+use rocket::response::stream::{Event, EventStream};
+use rocket::serde::{Deserialize, Serialize};
 use rocket::tokio::select;
-
-#[get("/world")]
-fn world() -> &'static str {
-    "Hello, World!"
-}
+use rocket::tokio::sync::broadcast::{channel, error::RecvError, Sender};
+use rocket::{Shutdown, State}; 
 
 #[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
@@ -22,7 +18,28 @@ struct Message {
     #[field(validate = len(..20))]
     pub username: String,
     pub message: String,
-} 
+}
+
+/// Returns an infinite stream of server-sent events. Each event is a message
+/// pulled from a broadcast queue sent by the `post` handler.
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&msg);
+        }
+    }
+}
 
 #[post("/message", data = "<form>")]
 fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
@@ -35,5 +52,6 @@ fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
 fn rocket() -> _ {
     rocket::build()
         .manage(channel::<Message>(1024).0)
-        .mount("/hello", routes![world])
+        .mount("/", routes![post, events])
+        .mount("/", FileServer::from(relative!("static")))
 }
