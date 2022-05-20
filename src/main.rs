00ac798b137332956a5219c49d12941bdadbeeec
kaustubh-s1 <shuklakaustubh84@gmail.com>
2022-05-21 01:00:18 +0530

    add front-end

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,7 @@ async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStrea
     }
 }
 
+/// Receive a message from a form submission and broadcast it to any receivers.
 #[post("/message", data = "<form>")]
 fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
     // A send 'fails' if there are no active subscribers.
@@ -48,7 +49,6 @@ fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
 }
 
 #[launch]
-
 fn rocket() -> _ {
     rocket::build()
         .manage(channel::<Message>(1024).0)
